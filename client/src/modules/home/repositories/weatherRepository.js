fe5519782dc6289baf904c92bd7b97b68273b5c1
Yoharu <jonathas_a.a@hotmail.com>
2021-07-24 20:47:58 -0300

    Corrige a linguagem de retorno da api do clima Foi inserido a linguagem pt-br e a unidade em celsius.

--- client/src/modules/home/repositories/weatherRepository.js
+++ client/src/modules/home/repositories/weatherRepository.js
@@ -4,10 +4,12 @@ const axios = api({
   baseURL: "https://api.openweathermap.org",
   params: {
     appid: "17828e69bc637271d188e0163ce58894",
+    lang: "pt_br",
+    units: "metric",
   },
 });
 
-export const getWeather = (localization = "Sao Paulo") =>
+export const getWeather = (localization = "Santo Andre") =>
   axios.get("/data/2.5/weather", {
     params: {
       q: localization,

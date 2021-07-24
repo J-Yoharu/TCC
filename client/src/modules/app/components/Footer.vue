efd5b689067bb6352248828867ee3e1644236a10
Yoharu <jonathas_a.a@hotmail.com>
2021-07-24 20:47:23 -0300

    Cria footer Feita a criação do componente footer e inserido em todas as telas.

---
+++ client/src/modules/app/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-footer padless>
+    <v-col class="text-center pa-0" cols="12">
+      <v-btn color="black" text rounded class="my-2"> Home </v-btn>
+      <v-btn color="black" text rounded class="my-2"> Contato </v-btn>
+      <v-btn color="black" text rounded class="my-2"> Quem somos </v-btn>
+    </v-col>
+    <v-col class="text-center pa-0" cols="12">
+      {{ new Date().getFullYear() }} —
+      <strong>TCC - Ciências da computação</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

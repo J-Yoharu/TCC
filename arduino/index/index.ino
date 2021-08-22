ea87784b82941fc585c72a6152fc4a9e8890c121
Yoharu <jonathas_a.a@hotmail.com>
2021-08-21 22:02:20 -0300

    Feita a modularização do index para classes específicas  - Feita a criação da classe firebase e waterMonitor para gerenciar o nível da água e o firebase.

--- arduino/index/index.ino
+++ arduino/index/index.ino
@@ -1,165 +1,95 @@
  
- // CONECTA COM O WI-FI
   #include <ESP8266WiFi.h>
-  // CRIA O SERVIDOR
   #include <ESP8266WebServer.h> 
-  #include <string>
   #include <WebSocketsServer.h>
+  #include <ESP8266WiFi.h>
+  #include "Firebase.h"
+  #include "WaterMonitor.h"
+
+  //get fingerprint from firebaseio.com https://www.grc.com/fingerprints.htm SÓ COLAR A URL DO SITE
+  #define FIREBASE_DATABASEURL "https://tcc-unip-4d803-default-rtdb.firebaseio.com/"
+  #define FINGERPRINT "50:89:50:57:90:1F:37:E3:B8:F3:5B:02:ED:3A:65:6E:6A:34:DB:93" //para sites HTTPS
 
   WebSocketsServer webSocket = WebSocketsServer(8081);
+
   //VALOR SERIAL P/ COMUNICAÇÃO
   #define valSerial 115200
-  //DEFINIÇÃO DOS LEDS E SEUS PINOS 
-  #define RedLed 0
-  #define YellowLed 4
-  #define WhiteLed 5
-  // buzzer 
-  #define buzzer 13
 
- // LIMITE DE DIST NCIA EM RELAÇÃO AO RESERVATÓRIO
-  #define limite1 40
-  #define limite2 80
-
-//DEFINIÇÃO DO SENSOR E SEUS PINOS
-  #define trigPin 12
-  #define echoPin 14
-
-// Dimensões do recipiente de teste
-
+  // Dimensões do recipiente de teste
   float pi = 3.14159;
   float altura = 8;
   float raio =  7;
   float total_volume = 0;
   float volume_water = 0;
+  bool first; 
+
+  Firebase firebase;
+  WaterMonitor monitor;
   
-  float distancia = 0;
   char data[5];
   
  //NOME E SENHA DA REDE WI-FI
-  const char *ssid ="Medina_Net";
-  const char *password ="120713Ss";
+  const char *ssid ="Jonabru";
+  const char *password ="11029701b";
 
-  bool first;
   ESP8266WebServer server(80);// objeto do servidor na porta 80, padrão
 
   void setup() {
-  
-/*==========CONFIGURAÇÃO DO SERVER*============*/
+    
   Serial.begin(valSerial);
   webSocket.begin();
-  Serial.print("Rede Wi-Fi: ");
-  Serial.println(ssid);
-  WiFi.begin(ssid,password);
-  
-//TENTANDO CONEXÃO COM WI-FI
-while(WiFi.status() != WL_CONNECTED){
-  Serial.print("Tentando conexão com a rede ");
-  Serial.println(WiFi.SSID());
-  delay(500);
-  }
-  Serial.print("CONECTADO COM SUCESSO! esse é o seu ip: ");
-  Serial.println(WiFi.localIP());
-  Serial.println("==============================================================");
+  connectWifi();
 
-/*==========Volume do recipiente*============*/  
+  firebase.init(FIREBASE_DATABASEURL,FINGERPRINT);
+  total_volume = monitor.volume_total(raio, altura, pi);
 
-total_volume = volume_total(raio, altura);
-
-/*==========CONFIGURAÇÃO DOS PINOS*============*/
-
- //MODO DOS LEDS
-  pinMode(RedLed,OUTPUT);
-  pinMode(YellowLed,OUTPUT);
-  pinMode(WhiteLed,OUTPUT);
-
-  //MODO DO SENSOR
-  pinMode(trigPin,OUTPUT);
-  pinMode(echoPin,INPUT);
-
-  // buzzer
-
-  pinMode(buzzer, OUTPUT);
-  noTone(buzzer);
 }
 
 void loop() {
         webSocket.loop();
-        // CONFIGURANDO O SENSOR DE DISTANCIA ULTRASSONICO
-        digitalWrite(trigPin, LOW);
-        delay(500);
-        digitalWrite(trigPin, HIGH);
-        delay(500);
-        digitalWrite(trigPin, LOW);
-        unsigned long duracao = pulseIn(echoPin, HIGH);
-        distancia = (duracao * 0.034)/2;
-        server.handleClient(); //CHAMANDO REQUISIÇÕES HTTP CONTINUAMENTE 
-        Serial.print("Distância em CM: ");
-        Serial.println(distancia);
-
-        volume_water = volume_of_water(distancia, altura, raio);
-        float percent = (volume_water /total_volume) * 100 ;
-        sendToSocket(percent);
-        Serial.println(percent);
+        String data = firebase.get("led");
+        Serial.print(data);
+    
+//        server.handleClient(); //CHAMANDO REQUISIÇÕES HTTP CONTINUAMENTE 
         
-
-                                  if(percent <= limite1 ){        
-
-                                        noTone(buzzer);
-                                        digitalWrite(WhiteLed, HIGH);
-                                        digitalWrite(RedLed, LOW);
-                                        digitalWrite(YellowLed, LOW);      
-                                  }else if ((percent <= limite2) && (percent >= limite1)){
-                   
-                                       digitalWrite(YellowLed, HIGH);   
-                                       digitalWrite(RedLed, LOW);
-                                       digitalWrite(WhiteLed, LOW);  
-
-                                       // ligando o buzzer
-                                       noTone(buzzer);
-                                       tone(buzzer,1000); 
-
-                                       // desligando o buzzer
-                                       delay(1000);
-                                       noTone(buzzer);  
-                                       delay(1000);   
-                                                                                                                                                     
-                                  }else {
-
-                                       digitalWrite(RedLed, HIGH);
-                                       digitalWrite(YellowLed, LOW);                                   
-                                       digitalWrite(WhiteLed, LOW);
-
-                                       // ligando o buzzer
-                                       tone(buzzer, 1000);
-                                        }      
-                                        
+//        float distance = getDistance();
+//        float percent = monitor.percent_volume_of_water(distance, altura, raio, total_volume, pi);
+//        
+//        monitor.alert(percent);
+//  
+//        Serial.print("Distância em CM: ");
+//        Serial.println(distance);
+//        Serial.println(percent);
+//        sendToSocket(percent);                                   
    }
-float volume_total(float raio, float altura) {
 
-   float area = 0;
-   float volume = 0;
-   
-   area = (pi * (raio * raio));
-   volume = area * altura;
 
-  return volume;
+float getDistance(){
+      // CONFIGURANDO O SENSOR DE DISTANCIA ULTRASSONICO
+      digitalWrite(trigPin, LOW);
+      delay(500);
+      digitalWrite(trigPin, HIGH);
+      delay(500);
+      digitalWrite(trigPin, LOW);
+      unsigned long duracao = pulseIn(echoPin, HIGH);
+      float distance = (duracao * 0.034)/2;
+      return distance;
 }
-
-float volume_of_water(float distancia, float altura, float raio) {
-
-  float water_height = 0;
-  float area = 0;
-  float volume_water = 0;
-  
-  water_height = altura - distancia;
-  area = (pi * (raio * raio));
-  volume_water = area * water_height;
-
-  return volume_water;
-  
-  }
 
 void sendToSocket(float value){
    char *valor = dtostrf(value,4,0,data);
     webSocket.broadcastTXT(valor);
+}
+
+
+void connectWifi()
+{
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("Conectado a: ");
+  Serial.print(WiFi.localIP());
 }
--- arduino/index/readme.md
+++ arduino/index/readme.md
@@ -1,3 +1,13 @@
+# Seguir corretamente as versões para não dar problema.
+
 ## setup da placa no link: https://blogmasterwalkershop.com.br/embarcados/wemos/wemos-d1-configurando-a-ide-do-arduino
 
-### install library websockets by Markus Sattler v 2.3.5
+Para instalar uma biblioteca, vá em `sketch >> Incluir biblioteca >> gerenciar bibliotecas` ou simplesmente aperte `ctrl + shift + i` que será direcionado direto para a tela de gerenciamento de bibliotecas.
+
+### Instalar biblioteca `websockets` by `Markus Sattler` v 2.3.5
+
+### Instalar biblioteca `ArduinoJson` by `Benoit Blanchon` v 5.13.4
+
+Para instalar uma placa, vá em `Ferramentas >> Placa >> Gerenciador de placas`
+
+### Instalar placa `esp8266` by `ESP8266 Community` v 2.7.4
